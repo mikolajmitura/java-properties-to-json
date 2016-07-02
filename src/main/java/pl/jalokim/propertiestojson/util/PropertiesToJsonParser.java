ae81ecb1601f9a29e76781a0deae0a7819639912
Mikołaj Mitura <mikolio1@o2.pl>
2016-07-02 20:02:44 +0200

    #1, #6 change names

--- src/main/java/pl/jalokim/propertiestojson/util/PropertiesToJsonParser.java
+++ src/main/java/pl/jalokim/propertiestojson/util/PropertiesToJsonParser.java
@@ -16,34 +16,38 @@ import static pl.jalokim.propertiestojson.util.exception.ParsePropertiesExceptio
 
 public class PropertiesToJsonParser {
 
-	public static final String DOT = "\\.";
+    public static final String DOT = "\\.";
 
-	public static String parseToJson(Map<String,String> prop){
-		ObjectJson objectJson = new ObjectJson();
-		for (String key: getAllKeyFromMap(prop)){
-			fetchValuesToJsonObject(prop, objectJson, key);
-		}
-		return objectJson.toStringJson();
-	}
+    public static String parseToJson(Map<String, String> properties) {
+        ObjectJson coreObjectJson = new ObjectJson();
+        for (String propertiesKey : getAllKeysFromMap(properties)) {
+            addFieldsToJsonObject(properties, coreObjectJson, propertiesKey);
+        }
+        return coreObjectJson.toStringJson();
+    }
 
-	private static void fetchValuesToJsonObject(Map<String, String> prop, ObjectJson objectJson, String key) {
-		String[] fields = key.split(DOT);
-		ObjectJson currentObjectJson = objectJson;
-		for (int index=0; index< fields.length; index++){
+    private static void addFieldsToJsonObject(Map<String, String> properties, ObjectJson coreObjectJson, String propertiesKey) {
+        String[] fields = propertiesKey.split(DOT);
+        ObjectJson currentObjectJson = coreObjectJson;
+        addFieldForCurrentJsonObject(properties, propertiesKey, fields, currentObjectJson);
+    }
+
+    private static void addFieldForCurrentJsonObject(Map<String, String> properties, String propertiesKey, String[] fields, ObjectJson currentObjectJson) {
+        for (int index = 0; index < fields.length; index++) {
             String field = fields[index];
 
-            if (isPrimitiveType(fields, index)){
-                addPrimitiveFieldWhenIsValid(prop, key, currentObjectJson, field);
+            if (isPrimitiveType(fields, index)) {
+                addPrimitiveFieldWhenIsValid(properties, propertiesKey, currentObjectJson, field);
             } else {
-                currentObjectJson = fetchJsonObjectOrCreate(key, currentObjectJson, field);
+                currentObjectJson = fetchJsonObjectOrCreate(propertiesKey, currentObjectJson, field);
             }
         }
-	}
+    }
 
     private static ObjectJson fetchJsonObjectOrCreate(String key, ObjectJson currentObjectJson, String field) {
-        if (alreadHasField(currentObjectJson, field)){
+        if (alreadHasField(currentObjectJson, field)) {
             currentObjectJson = fetchJsonObjectWhenIsNotPrimitive(key, currentObjectJson, field);
-        }else {
+        } else {
             currentObjectJson = createNewJsonObjectAndAssignToCurrent(currentObjectJson, field);
         }
         return currentObjectJson;
@@ -63,7 +67,7 @@ public class PropertiesToJsonParser {
     }
 
     private static void checkIsJsonObject(String key, AbstractJsonType jsonType) {
-        if (isNotJsonPrimitiveType(jsonType)){
+        if (isNotJsonPrimitiveType(jsonType)) {
             throw new ParsePropertiesException(String.format(UNEXPECTED_JSON_OBJECT, key));
         }
     }
@@ -72,20 +76,20 @@ public class PropertiesToJsonParser {
         return currentObjectJson.containsField(field);
     }
 
-    private static void addPrimitiveFieldWhenIsValid(Map<String, String> prop, String key, ObjectJson currentObjectJson, String field) {
-        checkThatFieldNotExistYet(key, currentObjectJson, field);
-        addPrimitiveFieldToCurrnetJsonObject(prop, key, currentObjectJson, field);
+    private static void addPrimitiveFieldWhenIsValid(Map<String, String> properties, String propertiesKey, ObjectJson currentObjectJson, String field) {
+        checkThatFieldNotExistYet(propertiesKey, currentObjectJson, field);
+        addPrimitiveFieldToCurrnetJsonObject(properties, propertiesKey, currentObjectJson, field);
     }
 
-    private static void checkThatFieldNotExistYet(String key, ObjectJson currentObjectJson, String field) {
-        if (alreadHasField(currentObjectJson, field)){
-            throw new ParsePropertiesException(String.format(UNEXPECTED_PRIMITIVE_TYPE, key));
+    private static void checkThatFieldNotExistYet(String propertiesKey, ObjectJson currentObjectJson, String field) {
+        if (alreadHasField(currentObjectJson, field)) {
+            throw new ParsePropertiesException(String.format(UNEXPECTED_PRIMITIVE_TYPE, propertiesKey));
         }
     }
 
-    private static void addPrimitiveFieldToCurrnetJsonObject(Map<String, String> prop, String key, ObjectJson currentObjectJson, String field) {
-        String propValue = prop.get(key);
-        if (isInteger(propValue)){
+    private static void addPrimitiveFieldToCurrnetJsonObject(Map<String, String> properties, String propertiesKey, ObjectJson currentObjectJson, String field) {
+        String propValue = properties.get(propertiesKey);
+        if (isInteger(propValue)) {
             currentObjectJson.addField(field, new IntegerJson(getInt(propValue)));
         } else {
             currentObjectJson.addField(field, new StringJson(propValue));
@@ -97,13 +101,13 @@ public class PropertiesToJsonParser {
     }
 
     private static boolean isPrimitiveType(String[] fields, int index) {
-        int lastIndex = fields.length-1;
-        return index==lastIndex;
+        int lastIndex = fields.length - 1;
+        return index == lastIndex;
     }
 
-    private static Set<String> getAllKeyFromMap(Map<String, String> prop) {
-		return prop.keySet();
-	}
+    private static Set<String> getAllKeysFromMap(Map<String, String> properties) {
+        return properties.keySet();
+    }
 
 
 }
