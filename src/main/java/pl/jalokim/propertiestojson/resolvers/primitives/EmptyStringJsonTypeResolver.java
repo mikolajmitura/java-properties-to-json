4e02d183d0cf1f303980697b7b375673f23db706
mmitura <mikolaj.mitura@igt.com>
2019-07-26 12:14:33 +0200

    added method for create multidimensional array during add some json object or value.

--- src/main/java/pl/jalokim/propertiestojson/resolvers/primitives/EmptyStringJsonTypeResolver.java
+++ src/main/java/pl/jalokim/propertiestojson/resolvers/primitives/EmptyStringJsonTypeResolver.java
@@ -1,15 +1,3 @@
-/*-
- * #%L
- * IGT Common Data Model
- * %%
- * Copyright (C) 2016 - 2017 IGT
- * %%
- * This software and all information contained therein is confidential and proprietary
- * and shall not be duplicated, used, disclosed or disseminated in any way except
- * as authorized by the applicable license agreement, without the express written permission of IGT.
- * All authorized reproductions must be marked with this language.
- * #L%
- */
 package pl.jalokim.propertiestojson.resolvers.primitives;
 
 import pl.jalokim.propertiestojson.resolvers.PrimitiveJsonTypesResolver;
